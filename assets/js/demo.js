cd192f17cf0e3432188aabf8943aa117d4e09b38
Jacob <jerling1989@gmail.com>
2018-01-04 21:25:52 -0500

    js comments

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -42,37 +42,26 @@
 }
 
 
-
+// MODAL JS
 $(document).ready(function() {
 
-// Gets the video src from the data-src on each button
+	// GETS VIDEO SRC FROM DATA-SRC ON EACH BTN
+	var $videoSrc;  
+	$('.video-btn').click(function() {
+	    $videoSrc = $(this).data( "src" );
+	});
+	console.log($videoSrc);
 
-var $videoSrc;  
-$('.video-btn').click(function() {
-    $videoSrc = $(this).data( "src" );
-});
-console.log($videoSrc);
-
-  
-  
-// when the modal is opened autoplay it  
-$('#myModal').on('shown.bs.modal', function (e) {
+	// CREATE AUTOPLAY WHEN MODAL IS OPENED
+	$('#myModal').on('shown.bs.modal', function (e) {
+	    
+	// REMOVE SUGGESTED VIDEOS AT END OF CLIP
+	$("#video").attr('src',$videoSrc + "?rel=0&amp;showinfo=0&amp;modestbranding=1&amp;autoplay=1" ); 
+	})
+	   
+	// STOP PLAYING VIDEO WHEN MODAL IS CLOSED
+	$('#myModal').on('hide.bs.modal', function (e) {
+	  $("#video").attr('src',$videoSrc); 
+	}) 
     
-// set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
-$("#video").attr('src',$videoSrc + "?rel=0&amp;showinfo=0&amp;modestbranding=1&amp;autoplay=1" ); 
-})
-  
-  
-// stop playing the youtube video when I close the modal
-$('#myModal').on('hide.bs.modal', function (e) {
-    // a poor man's stop video
-    $("#video").attr('src',$videoSrc); 
-}) 
-    
-    
-
-
-  
-  
-// document ready  
 });
